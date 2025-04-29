763910a30db8cd08c845fc774bede61e337506d5
Hasturious <Jbster559@gmail.com>
2025-04-28 19:16:00 -0700

    Add project files.

---
+++ COMP003B.LectureActivity6C/Models/Enrollment.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.LectureAcvtivity6.Models
+{
+    public class Enrollments 
+    {
+        public int ID { get; set; }
+        public string StudentID { get; set; }
+        public string CourseID { get; set; }
+
+        //collection navigation property
+        //public virtual ICollection<Enrollment>? Enrollments { get; set; }
+    }
+}
